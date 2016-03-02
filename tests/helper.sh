4f16940fa867cadfa33b492ffbfe412189104259
Carlos Alexandro Becker <caarlos0@gmail.com>
2016-03-01 21:24:44 -0300

    shellcheck

--- tests/helper.sh
+++ tests/helper.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # setups a fake local repository
 fakerepo() {
-  cd "$(mktemp -d)"
+  cd "$(mktemp -d)" || exit
   git init -q
   git remote add origin git@github.com:fake/fake.git
   pwd
